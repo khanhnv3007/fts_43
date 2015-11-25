02b8b615e3f27844ad495040b99beedf4ecb87d2
khanhnv3007 <khanhnv3007@gmail.com>
2015-11-25 13:42:46 +0700

    User can index, delete, edit their questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,6 +15,34 @@ class QuestionsController < ApplicationController
     redirect_to :back
   end
 
+  def index
+    @questions = current_user.questions
+  end
+
+  def edit
+    @subjects = Subject.all
+    @question.answers.build
+  end
+
+  def update
+    if @question && @question.update_attributes(question_params)
+      flash[:sucess] = t "questions.success"
+      redirect_to user_questions_path current_user
+    else
+      flash[:danger] = t "questions.fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @question.destroy
+      flash[:success] = t "questions.success"
+    else
+      flash.now[:danger] = t "questions.fail"
+    end
+    redirect_to :back
+  end
+
   private
   def question_params
     params.require(:question).permit :content, :subject_id, :user_id,
